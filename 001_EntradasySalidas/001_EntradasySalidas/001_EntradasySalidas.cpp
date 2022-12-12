193f6d70dfec464387bc2489c45baa24ce0dce83
Pepe2505 <pepe8alzojeda@gmail.com>
2022-12-12 12:46:43 -0600

    entradas y salidas
    codigo completo

---
+++ 001_EntradasySalidas/001_EntradasySalidas/001_EntradasySalidas.cpp
@@ -0,0 +1,15 @@
+// Lectura o Entrada de salida
+
+#include <iostream>
+
+int main()
+{
+	int numero;
+    
+	std::cout << "Digite un mumero:";
+	std::cin >> numero;
+
+	std::cout << "\n El munero que digito es:" << numero << std::endl;
+
+	return 0;
+}
