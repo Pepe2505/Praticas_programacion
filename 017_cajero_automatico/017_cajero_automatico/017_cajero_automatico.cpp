c22b5f382d92f66d28501f8d64b9900883e87669
Pepe2505 <pepe8alzojeda@gmail.com>
2022-12-19 10:30:08 -0600

    017_cajero_automatico

--- 017_cajero_automatico/017_cajero_automatico/017_cajero_automatico.cpp
+++ 017_cajero_automatico/017_cajero_automatico/017_cajero_automatico.cpp
@@ -6,7 +6,10 @@ de 1000 dolares */
 int main()
 {
 	int saldo_inicial = 1000, opc;
-	float extra, saldo;
+	float extra, saldo, retiro;
+
+	int numero, cubo;
+	float resultado = 0;
 
 	std::cout << "\t Bienbenido a su casero automatico";
 	std::cout << "1. ingrese dinero en tu cuenta ";
@@ -16,15 +19,55 @@ int main()
 
 	switch (opc)
 	{
-	  case 1:
-		  std::cout << "Digite la cantidad de dinero a ingresar: ";
-		  std::cin >> extra;
-		  saldo = saldo_inicial + extra;
-		  std::cout << "dinero en cuenta";
-		  break;
-	  case 2:
-	  case 3:
+	case 1:
+		std::cout << "\n Digite la cantidad de dinero a ingresar: ";
+		std::cin >> extra;
+		saldo = saldo_inicial + extra;
+		std::cout << "dinero en cuenta" << saldo;
+		break;
+	case 2:
+		std::cout << "\n Digite la cantidad de dinero que va a retirar: ";
+		std::cin >> retiro;
+		if (retiro > saldo_inicial)
+		{
+			std::cout << "no tiene esa cantidad de dinero";
+		}
+		else {
+			saldo = saldo_inicial - retiro;
+			std::cout << "Dinero en cuenta:" << saldo;
+		}
+	case 3:
+		break;
 	}
+
+	std::cout << "\n en este apartado vas a poder realizar 3 coasas";
+	std::cout << "las tres opciones son  sacar el cubo de un numero, ver si un numero es par o es inpar y salir (1 es sacar el cubo de un numero, 2 ver si es par o no y 3 es salir)";
+	std::cin >> opc;
+
+	switch (opc)
+	{
+	  case 1:
+		std::cout << "\n a que numero le quieres sacar al cubo: ";
+		std::cin >> numero;
+		cubo = pow(numero, 3);
+		std::cout << "su cubo de este numero es:" << cubo << std::endl;
+		break;
+	  case 2:
+		  std::cout << "\n que numero quieres saber si es par o no";
+		  std::cin >> numero;
+		  if(numero%2 ==0)
+		  {
+			  std::cout << "\n el numero es par";
+			  std::cout << "\n ya que este numero es divisible entre 2:" << numero << std::endl;
+		  }
+		  else {
+			  std::cout << "\n este numero es inpar";
+			  std::cout << "\n ya que este numero no es divisible entre 2: " << numero << std::endl;
+		  }
+	  case 3:
+		  std::cout << "\n vamos a salir de este pograma";
+	}
+
 
 	return 0;
 }
