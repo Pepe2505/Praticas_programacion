c944b03d35f691e0ceaecbb0506cb3c59d2e96b6
Pepe2505 <pepe8alzojeda@gmail.com>
2022-12-19 12:10:11 -0600

    020
    termine la pratica

---
+++ 020_for/020_for/020_for.cpp
@@ -0,0 +1,20 @@
+/* La stencia for
+for (expr1; expresion logica; expr 2) {
+ conjunto de instruciones;
+}
+*/
+
+#include <iostream>
+#include <conio.h>
+
+int main() 
+{
+	int i;
+
+	for(i=1; i<=10; i++)
+	{
+		std::cout << i << std::endl;
+	}
+
+	return 0;
+}
