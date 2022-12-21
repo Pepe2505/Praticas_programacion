2c294000503f6aa5c0cab75d3377e55f35ca39b6
Pepe2505 <pepe8alzojeda@gmail.com>
2022-12-21 10:22:03 -0600

    025
    pratica terminada

--- 025_fibonacci/025_fibonacci/025_fibonacci.cpp
+++ 025_fibonacci/025_fibonacci/025_fibonacci.cpp
@@ -5,6 +5,8 @@
 int main()
 {
 	int n, x = 0, y = 1, z = 1;
+	int alumnos_que_aprovaron_todos_los_examenes, aprovaron_solo_un_examen, aprovaron_el_ultimo_examen, cantidad_de_examenes_que_aprovaron_todos, total_de_examenes_aprovados, alumnos;
+	int examenes_por_alumno;
 
 	std::cout << "digite el numero de elementos: ";
 	std::cin >> n;
@@ -18,7 +20,22 @@ int main()
 		y = z; //1 2 3
 	}
 
+	std::cout << "\n cuantos alumnos aprovaron todos los examenes";
+	std::cin >> alumnos_que_aprovaron_todos_los_examenes;
+	std::cout << "cuantos aprovaron solo un";
+	std::cin >> aprovaron_solo_un_examen;
+	std::cout << "cuantps aprovaron el ultimo examen";
+	std::cin >> aprovaron_el_ultimo_examen;
+	std::cout << "cuantos alumnos no reprobaron ningun examen";
+	std::cin >> alumnos;
+	std::cout << "cauntos examenes hizo cada alumno: ";
+	std::cin >> examenes_por_alumno;
 
+	cantidad_de_examenes_que_aprovaron_todos = alumnos * examenes_por_alumno;
+	total_de_examenes_aprovados = cantidad_de_examenes_que_aprovaron_todos + aprovaron_solo_un_examen;
+
+	std::cout << "\n el total de los examenes que aprovaron los alumnos que aprovaron todos los examenes: " << cantidad_de_examenes_que_aprovaron_todos << std::endl;
+	std::cout << "total de examenes aprovados: " << total_de_examenes_aprovados << std::endl;
 
 	return 0;
 }
