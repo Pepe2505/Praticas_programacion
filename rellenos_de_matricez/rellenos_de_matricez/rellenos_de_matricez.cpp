2b89a833e542cd2564a5aeeba3e690e2b5ff1c63
Pepe2505 <pepe8alzojeda@gmail.com>
2022-12-22 12:30:52 -0600

    031
    matricez

---
+++ rellenos_de_matricez/rellenos_de_matricez/rellenos_de_matricez.cpp
@@ -0,0 +1,28 @@
+/*1. Hacer un pograma para rellenar una matriz pidendo el numero de filas
+y columnas, posteriormente mostraz'r la matriz en pantalla. */
+
+#include <iostream>
+#include <conio.h>
+
+int main()
+{
+	int numeros[100][100], filas, columnas;
+
+	std::cout << "digite el numero de filas: ";
+	std::cin >> filas;
+	std::cout << "digite el numero de columna: ";
+	std::cin >> columnas;
+
+	for(int i=0; i<filas; i++)
+	{
+		for(int j=0; j<columnas; j++ )
+		{
+			std::cout << "Digite un numero [" << i << "]";
+			std::cin >> numeros[i][j];
+		}
+		std::cout << "\n";
+	}
+
+	getch();
+	return 0;
+}
